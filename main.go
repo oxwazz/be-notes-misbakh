5825c6f69e6e7a6a77ac16d0dfa49f2feedc3c6a
oxwazz <muhammad.rahmahalim@gmail.com>
2024-03-17 13:55:08 +0700

    fix: add cors

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
@@ -78,6 +79,8 @@ func main() {
 	}
 
 	e := echo.New()
+	e.Use(middleware.CORS())
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!!")
 	})
